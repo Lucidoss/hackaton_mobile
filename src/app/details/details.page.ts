912d2b043c9bffde2aa93213d460246e95ff6988
Remery <romain.dugas85@gmail.com>
2022-11-18 15:36:28 +0100

    page details sans le style

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -14,9 +14,20 @@ export class DetailsPage {
   constructor(private router: Router, private activeRoute : ActivatedRoute) { 
     this.activeRoute.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation()) {
-        this.hackathon = this.router.getCurrentNavigation().extras.state.param1
+        this.hackathon = this.router.getCurrentNavigation()?.extras.state
+        console.log(this.hackathon.param1)
       }
     })
+  }
+
+  jsontoDate(jsonDate:any) {
+    var date = new Date(jsonDate)
+    var realDate = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear()
+    if ((date.getMonth()+1) < 10) {
+      realDate = date.getDate() + '/' + '0'+(date.getMonth()+1) + '/' + date.getFullYear()
+    }
+    
+    return realDate
   }
 
 }
